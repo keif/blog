c2ac941f7a90263f4b9de98aee4cd952bc7c08c7
Keith T. Baker <hello@keif.name>
2020-07-22 21:59:30 -0400

    adding state in context

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,14 +1,19 @@
-import React from "react"
+import React, {useState} from "react"
 
 const BlogContext = React.createContext()
 
 export const BlogProvider = ({children}) => {
-    const blogPosts = [
-        { id: 1, title: "Blog Post #1" },
-        { id: 2, title: "Blog Post #2" },
-    ]
+    const [blogPosts, setBlogPosts] = useState([])
+
+    const addBlogPost = () => {
+        setBlogPosts([...blogPosts, {
+            id: `${blogPosts.length + 1}`,
+            title: `Blog Post #${blogPosts.length + 1}`,
+        }])
+    }
+
     return (
-        <BlogContext.Provider value={blogPosts}>
+        <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
             {children}
         </BlogContext.Provider>
     )
